0e32252c3fbc47708ae5e4a1a73d07f19e679376
刘其朋 <pass1234>
2020-07-13 19:46:43 +0800

    init-----搭建框架gateway、registry、item的一部分

---
+++ ly-api-gateway/src/main/java/com/leyou/gateway/LyApiGateway.java
@@ -0,0 +1,20 @@
+package com.leyou.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author itw_liuqp
+ * @date 2020/7/13 14:20
+ * @describe
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class,args);
+    }
+}
