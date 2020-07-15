4fb3b0eb6c5d8e7e3f2c8ff97007c3ba4af51e56
刘其朋 <pass1234>
2020-07-15 17:44:32 +0800

    完善框架，遇到的问题是item配置数据源的时候有点问题，还有就是cloud与boot版本不匹配

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/service/item/LyItemService.java
@@ -0,0 +1,18 @@
+package com.leyou.service.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author itw_liuqp
+ * @date 2020/7/14 10:49
+ * @describe
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class,args);
+    }
+}
